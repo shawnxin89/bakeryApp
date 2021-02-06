ac0728ba4c210df4d13bb8344a2d768226c220c6
shawnxin89 <xzixing@seelenet.com>
2021-02-05 21:38:14 -0800

    some fixes

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 My friend asked me to build a Web service for her bakery business (doing pretty well). So here is one.
+
+The backend service is built with typescript + Express. 
+
+AWS RDS service is used for the storage of the order information.
+
--- src/config/config.ts
+++ src/config/config.ts
@@ -9,21 +9,11 @@ export const config = {
         "aws_profile": process.env.AWS_PROFILE || "g",
         "aws_media_bucket": process.env.AWS_MEDIA_BUCKET || "h"
       }
-    // "dev": {
-    //   "username": "admin",
-    //   "password": "birdiebakery",
-    //   "database": "bakerySchema",
-    //   "host": "bakerydb.cuxk21dnawke.us-west-1.rds.amazonaws.com",
-    //   "dialect": "mysql",
-    //   "aws_region": "us-west-1",
-    //   "aws_profile": "default",
-    //   "aws_media_bucket": "bakery-storage-dev"
-    // }
     
     // "prod": {
     //   "username": "",
     //   "password": "",
-    //   "database": "udagram_prod",
+    //   "database": "",
     //   "host": "",
     //   "dialect": "postgres"
     // }
